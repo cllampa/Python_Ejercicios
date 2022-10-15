50b2a1f9e104e41c3fd4be1c7d084e0daa145da2
Jose Cristian Llampa <cllampa3@gmail.com>
2022-10-15 09:18:27 -0300

    Update Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,4 +1,4 @@
-peso=float(input("Ingrese su peso "))
-altura=float(input("Ingrese su altura "))
+peso=float(input("Ingrese su peso en Kg "))
+altura=float(input("Ingrese su altura metros"))
 imc=float(peso/(altura*altura))
 print("Tu indice de masa corporal es ",round(imc,2))
